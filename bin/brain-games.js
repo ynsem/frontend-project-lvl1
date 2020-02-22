870c3c564d718567b35f52dd63af140d63d7f9c9
ynsem <y.n.semenchenko@mail.ru>
2020-02-23 01:42:42 +0300

    passes linter and adds a github actions badge

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { greetUser } from '../src/index.js'
+import greetUser from '../src/index.js';
 
 console.log('Welcome to the Brain Games!');
 greetUser();
--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import readlineSync from 'readline-sync';
 
 const getUsername = () => readlineSync.question('May I have your name? ');
 
-export const greetUser = () => console.log(`Hello, ${getUsername()}!`)
+const greetUser = () => console.log(`Hello, ${getUsername()}!`);
+
+export default greetUser;
