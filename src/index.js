4f7f07b0e5dad51270430c737ee4c221a6f4c0c4
ynsem <y.n.semenchenko@mail.ru>
2020-03-07 17:18:26 +0300

    меняет порядок приветствия, убирает greetUser

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import readlineSync from 'readline-sync';
 
-// для brain-games
-const getUsername = () => readlineSync.question('May I have your name? ');
-
-// имя пользователя
-let userName = '';
-
-export const greetUser = () => {
-  console.log('Welcome to the Brain Games!');
-  userName = getUsername();
-  console.log(`Hello, ${userName}!`);
-};
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}!`);
 
 // скопировано с https://learn.javascript.ru/task/random-int-min-max
 export const getRandomInt = (min, max) => Math.round(min - 0.5 + Math.random() * (max - min + 1));
@@ -30,8 +21,7 @@ export const startRound = (gameExpression, trueAnswer) => {
 };
 
 export const play = (gameParams, playGame) => {
-  greetUser();
-
+  console.log('Welcome to the Brain Games!');
   console.log(gameParams.rules);
 
   for (let i = 0; i < gameParams.rounds; i += 1) {
